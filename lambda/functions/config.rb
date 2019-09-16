0e9eb3d86c30a76c683b3e2acead08f773e575d8
David Joaquín Shourabi Porcel <david@djsp.eu>
2019-09-16 22:01:35 +0200

    FIXME

--- lambda/functions/config.rb
+++ lambda/functions/config.rb
@@ -13,12 +13,44 @@ def parse(yaml)
 end
 
 
+def validate(config)
+  return true
+end
+
+
+def main(yaml)
+  valid = False
+  config = None
+
+  parsing_messages = []
+
+  begin
+    config = parse(
+      yaml,
+    )
+  rescue
+    status = 'INVALID'
+  else
+    validation_messages = []
+
+    response = validate(
+      config,
+    )
+  end
+
+  response = {
+  }
+
+  return response
+end
+
+
 def handler(event, context)
   yaml = event['YAML']
 
-  config = parse(
+  response = main(
     yaml,
   )
 
-  return config
+  return response
 end
